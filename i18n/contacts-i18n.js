a93b98fa8b025d967d8022dda3d2613a84217080
Xian Liu <lxjsj@foxmail.com>
2017-02-02 16:28:49 +0800

    DEV: Add Boogu en/zh values exchange

--- i18n/contacts-i18n.js
+++ i18n/contacts-i18n.js
@@ -1,2 +1,188 @@
 var langSwitch = langSwitch || {}
-langSwitch = $.extend(langSwitch, {});
+langSwitch = $.extend(langSwitch, {
+    "CONTACT-US-LINE1-TEXT": {
+        "zh": "北京吉诺恒扬网络技术有限公司",
+        "en": "Beijing Jino Technology Co., Ltd."
+    },
+    "CONTACT-US-LINE1-TEXT": {
+        "zh": "公司地址：北京市朝阳区建国门外大街22号赛特大厦二层",
+        "en": "Company Address：2nd Floor,Scitech Tower,22 Jianguomenwai Avenue,Chaoyang, Beijing, China"
+    },
+    "CONTACT-US-LINE2-TEXT": {
+        "zh": "电话：010-65159281",
+        "en": "Tel：010-65159281"
+    },
+    "CONTACT-US-LINE3-TEXT": {
+        "zh": "传真：010-65158879",
+        "en": "Fax：010-65158879"
+    },
+    "CONTACT-US-LINE4-TEXT": {
+        "zh": "邮箱： contact@Jinoinc.com",
+        "en": "Email： contact@Jinoinc.com"
+    },
+    "CONTACT-US-LINE5-TEXT": {
+        "zh": "联系我们:",
+        "en": "Contact Us:"
+    },
+    "CONTACT-US-LINE6-TEXT": {
+        "zh": "Boogu：boogu@Jinoinc.com",
+        "en": "Boogu：boogu@Jinoinc.com"
+    },
+    "CONTACT-US-LINE7-TEXT": {
+        "zh": "E-commerce： sales@Jinotrader.com",
+        "en": "E-commerce： sales@Jinotrader.com"
+    },
+    "CONTACT-US-LINE8-TEXT": {
+        "zh": "IMSG： IMSG@Jinoinc.com",
+        "en": "IMSG：IMSG@Jinoinc.com"
+    },
+    "CONTACT-US-LINE9-TEXT": {
+        "zh": "技术支持： support@Jinoinc.com",
+        "en": "Technical Support： support@Jinoinc.com"
+    },
+    "ABOUT-US-LINE1-TEXT" : {
+        "zh" : " 北京吉诺恒扬网络技术有限公司是一家致力于提供社会化媒体整合管理平台与舆情监测系统——布谷（Boogu），电子商务解决方案和服务——JINO Trader，全球内网管理系统IMSG（Intranet Management System Globalization）等相关技术开发与维护的技术公司。公司成立于2004年，前后合作过的客户主要为网络广告代理公司，公关公司，科技公司及一些大型的国企和世界五百强的企业，业务遍及北京、上海、深圳、香港等地。",
+        "en" : "Beijing Jino Technology Co., Ltd. is a commitment to provide social media integration management platform and public opinion monitoring system - Cog (Boogu), e-commerce solutions and services - JINO Trader, the global network management system IMSG (Intranet Management System Globalization), and other related technology development and maintenance of technology companies. Founded in 2004, before and after the cooperation of the main customers for the online advertising agency, public relations companies, technology companies and some large state-owned enterprises and the world's top 500 enterprises in Beijing, Shanghai, Shenzhen, Hong Kong and other places."
+    },
+    "ABOUT-US-LINE2-TEXT" : {
+        "zh" : "公司目前有大约40多人的技术团队，骨干成员为有多年工作经验的PM,系统架构师，高级工程师等，在团队的共同努力下，我们多年来不但积累了无数的客户，也拥有了完善的产品和丰富的项目执行经验。",
+        "en" : "The company currently has about 40 people in the technical team, the backbone members of the PM for many years of work experience, system architects, senior engineers, the joint efforts of the team, we have accumulated over the years not only countless customers, but also has improved Of products and a wealth of experience in project implementation."
+    },
+    "ABOUT-US-LINE3-TEXT" : {
+        "zh" : "公司的发展策略一直是与渠道客户合作，我们能长期以来获得我们的客户的大力支持，除了我们拥有完整的技术团体与开发维护的经验外，不与渠道客户直接竞争和维护渠道客户的利益，也是我们能一直获得客户认可的重要原因。",
+        "en" : "The company's development strategy has been working with channel customers, we have long been strong support of our customers, in addition to our complete technical group and the development and maintenance experience, not directly with customers and channel customers to maintain the interests of channels, But also we can always get customer recognition of the important reasons."
+    },
+    "ABOUT-US-LINE4-TEXT" : {
+        "zh" : "我们欢迎各种能与我们互惠互利的公司和我们合作，相信通过我们的合作，一定能为彼此带来更多的业务与更多的发展机会。",
+        "en" : "We welcome all companies and we can mutually beneficial cooperation with us, I believe that through our cooperation, we will be able to bring each other more business and more opportunities for development."
+    },
+
+    // privacy content start
+    "COMMITMENT-TITLE-TEXT" : {
+        "zh" : "承诺：",
+        "en" : "Commitment:"
+    },
+
+    "COMMITMENT-LINE1-CONTENT-TEXT" : {
+        "zh" : "我们致力于保护您在使用本网站时所提供的私隐、私人资料以及个人的资料（统称\"个人资料\"），除非根据法律或政府的强制性规定，在未得到您的许可之前，我们不会把您的任何个人信息提供给无关的第三方（包括公司或个人）。",
+        "en" : "We are committed to protecting the privacy, private data, and personal data (collectively, \"Personal Data\") that you provide in connection with your use of the Site, except as required by law or government without your prior permission. We will not provide any of your personal information to unrelated third parties (including companies or individuals)."
+    },
+
+    "COMMITMENT-LINE2-CONTENT-TEXT" : {
+        "zh" : "您可以选择是否披露信息。有些信息是登记我们的服务所必需的，但大多数其他信息的提供是由用户决定的。",
+        "en" : "You can choose whether to disclose the information. Some information is required to register our services, but most other information is provided by the user."
+    },
+
+    "COMMITMENT-LINE3-CONTENT-TEXT" : {
+        "zh" : "为确保您对网站在处理个人资料上有充分信心，请阅读及理解隐私政策的条文。您一旦使用本网站，将被视为接受、同意、承诺和确认：",
+        "en" : "To ensure that you have full confidence in the processing of your personal data, please read and understand the provisions of the Privacy Policy. By using this website, you will be deemed to accept, agree, promise and confirm that:"
+    },
+
+    "COMMITMENT-LINE4-CONTENT-TEXT" : {
+        "zh" : "您在自愿下连同所需的同意向我们披露个人资料；",
+        "en" : "You voluntarily disclose the personal data to us with the consent required;"
+    },
+
+    "COMMITMENT-LINE5-CONTENT-TEXT" : {
+        "zh" : "您会遵守本隐私政策的全部条款和限制；",
+        "en" : "You will comply with all the terms and conditions of this Privacy Policy;"
+    },
+
+    "COMMITMENT-LINE6-CONTENT-TEXT" : {
+        "zh" : "您在本网站上作登记、资料会被收集；",
+        "en" : "You are registered on this website, the data will be collected;"
+    },
+
+    "COMMITMENT-LINE7-CONTENT-TEXT" : {
+        "zh" : "您同意日后我们对隐私政策的任何修改。",
+        "en" : "You agree to any future changes to our privacy policy."
+    },
+
+    "COMMITMENT-LINE8-CONTENT-TEXT" : {
+        "zh" : "经您同意，我们会收集、管理和监控个人资料。为了向您提供我们的各项服务，您需要提供相关个人资料及一些与个人身份无关的信息。目的及用途如下：",
+        "en" : "We collect, manage and monitor personal data as you agree. In order to provide you with our services, you will need to provide relevant personal information and information that is not related to your personal identity. The purposes and uses are as follows:"
+    },
+
+    "COMMITMENT-LINE9-CONTENT-TEXT" : {
+        "zh" : "经您同意，我们会收集、管理和监控个人资料。为了向您提供我们的各项服务，您需要提供相关个人资料及一些与个人身份无关的信息。目的及用途如下：",
+        "en" : "We collect, manage and monitor personal data as you agree. In order to provide you with our services, you will need to provide relevant personal information and information that is not related to your personal identity. The purposes and uses are as follows:"
+    },
+
+    "COMMITMENT-LINE10-CONTENT-TEXT": {
+        "zh": "通过本网站向您提供我们的各项服务；",
+        "en": "Through this site to provide you with our services;"
+    },
+    "COMMITMENT-LINE11-CONTENT-TEXT": {
+        "zh": "当您使用本网站时，能辨认及确认您的身份；",
+        "en": "When you use this site, you can identify and confirm your identity;"
+    },
+    "COMMITMENT-LINE12-CONTENT-TEXT": {
+        "zh": "让您使用本网站时得到为您而设的感受；",
+        "en": "Let you use this site to get the feeling for you;"
+    },
+    "COMMITMENT-LINE13-CONTENT-TEXT": {
+        "zh": "我们有需要时可以与您联系；",
+        "en": "We need to contact you;"
+    },
+    "COMMITMENT-LINE14-CONTENT-TEXT": {
+        "zh": "统计本网站使用量的数据；",
+        "en": "Statistics on the use of this site data;"
+    },
+    "COMMITMENT-LINE15-CONTENT-TEXT": {
+        "zh": "令您使用本网站时更方便；",
+        "en": "So that you use this site more convenient;"
+    },
+    "COMMITMENT-LINE16-CONTENT-TEXT": {
+        "zh": "为改进产品、服务及网站内容而进行市场研究调查；",
+        "en": "Conduct market research to improve the products, services and website content;"
+    },
+    "COMMITMENT-LINE17-CONTENT-TEXT": {
+        "zh": "与我们合作的第三者进行产品及/或服务的推广；",
+        "en": "And third parties to cooperate with us in the promotion of products and / or services;"
+    },
+    "COMMITMENT-LINE18-CONTENT-TEXT": {
+        "zh": "其他符合相关法律法规的用途。",
+        "en": "Other uses in compliance with relevant laws and regulations."
+    },
+
+    "COOKIES-TITLE-TEXT" : {
+        "zh" : "Cookies",
+        "en" : "Cookies"
+    },
+
+    "COOKIES-CONTENT-TEXT" : {
+        "zh" : "使用Cookies是为了让其用户受益，Cookies能帮助我们确定您连接的页面和内容；Cookies只能被设置它们的服务器阅读，而且不能执行任何代码或病毒；Cookies使得我们能更好、更快地为您服务。如果您的浏览器被设置为拒绝cookies，您仍然能够访问我们的大多数页面。",
+        "en" : "Cookies help us determine the pages and content you connect to; Cookies can only be set to read on their servers, and can not execute any code or virus; Cookies enable us to better, faster for the user to be able to Your service. If your browser is set to refuse cookies, you will still be able to access most of our pages."
+    },
+
+    "UPDATES-AND-PUBLIC-INFORMATION-TITLE-TEXT" : {
+        "zh" : "更新与公开信息",
+        "en" : "Updates and public information"
+    },
+
+    "UPDATES-AND-PUBLIC-INFORMATION-CONTENT-LINE1-TEXT" : {
+        "zh" : "如果您的相关信息发生变化，我们鼓励您更新和修改以使其有效。您可以自行决定修改、删除他们的相关资料。",
+        "en" : "If your information changes, we encourage you to update and modify it to be effective. You may decide to modify or delete their information."
+    },
+
+    "UPDATES-AND-PUBLIC-INFORMATION-CONTENT-LINE2-TEXT" : {
+        "zh" : "请记住，无论何时您自愿披露个人信息（如在留言板、聊天区等网络公开场合公布）的，此种信息可能被他人收集及使用，因此造成您的个人信息泄露，本网站不承担责任。因此，我们提醒并请您慎重考虑是否有必要在上述渠道公开您的个人信息。",
+        "en" : "Keep in mind that whenever you voluntarily disclose your personal information (such as in public forums such as bulletin boards, chat rooms, etc.), this information may be collected and used by others, thus causing your personal information to be disclosed. responsibility. Therefore, we caution you to consider carefully whether it is necessary to disclose your personal information in the above-mentioned channels."
+    },
+
+    "UPDATES-AND-PUBLIC-INFORMATION-CONTENT-LINE3-TEXT" : {
+        "zh" : "最后，您是唯一对您的账号和密码信息负有保密责任的人。任何情况下，请小心妥善保管。",
+        "en" : "Finally, you are the only person who is responsible for keeping your account and password information confidential. In any case, please be careful and safe custody."
+    },
+
+    "PRIVACY-CONTACT-US-TITLE-TEXT" : {
+        "zh" : "联系我们",
+        "en" : "Contact us"
+    },
+
+    "PRIVACY-CONTACT-US-CONTENT-TEXT" : {
+        "zh" : "若您对我们的隐私政策有任何问题，欢迎随时联系我们",
+        "en" : "If you have any questions about our privacy policy, please do not hesitate to contact us"
+    }
+    // privacy content end
+});
